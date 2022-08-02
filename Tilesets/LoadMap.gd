91941f72a1e3de924ef9a74fcff2de99c2ef44a5
Louise Hsu <louiseh0313@gmail.com>
2022-08-02 00:29:57 -0700

    basic map loading capabilities

--- Tilesets/LoadMap.gd
+++ Tilesets/LoadMap.gd
@@ -6,9 +6,20 @@ var _tileset
 func _ready():
 	_tileset = get_tileset()
 	set_process_input(true)
+	
+	var map = load("res://Maps/map.png")
+	var data = map.get_data()
+	data.lock()
+	
+	
 
-	for x in range(32):
-		for y in range(32):
-			set_cellv(Vector2(x, y), 0)
+	for x in range(data.get_width()):
+		for y in range(data.get_height()):
+			var pixel = data.get_pixel(x ,y)
+			if (pixel[0] == 0) :
+				set_cellv(Vector2(x, y), 0)
+
 			
 	update_bitmask_region()
+	
+	
