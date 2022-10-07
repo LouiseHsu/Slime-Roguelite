f5292b613542b2bea32ac46ccf7613ef88a5ffd0
Louise Hsu <louiseh0313@gmail.com>
2022-10-06 23:26:06 -0700

    standardized weapon names

--- Player_Stats.gd
+++ Player_Stats.gd
@@ -38,15 +38,6 @@ var slot_info = {
 },
 "points" : 10 
 }
-	
-func add_weapon(weapon):
-	var wep_struct = {
-		"name" : weapon,
-		"level" : 1
-	}
-	
-	owned_weapons.append(wep_struct);
-	emit_signal("weapons_update");
 
 func take_damage(damage):
 	health -= damage;
--- Weapons.gd
+++ Weapons.gd
@@ -1,49 +1,34 @@
 extends Node2D
 
 const blue_orb = preload("res://Weapons/Blue_Orb.tscn");
-const shield_weapon = preload("res://Weapons/Shield_Weapon.tscn");
+const shield_weapon = preload("res://Weapons/Pink_Orb.tscn");
 
 const ALL_WEAPONS = {
 	"blue_orb": blue_orb,
-	"shield_weapon": shield_weapon
+	"pink_orb": shield_weapon
 }
 
 var weapon_list = {
 	"blue_orb": null,
-	"shield_weapon": null
+	"pink_orb": null
 }
 
 func _ready():
-	
-#	add_weapon("blue_orb");
-#	add_weapon("shield_weapon");
+	PlayerStats.connect("weapons_update", self, "update_weapons");
 	update_weapons()
-	print("added first weapon")
 
-func add_weapon(chosen_weapon):
-	if (weapon_list[chosen_weapon]):
-		weapon_list[chosen_weapon].level_up();
-		PlayerStats.level_up_weapon(chosen_weapon);
-		
-	else:
-		var weapon = ALL_WEAPONS[chosen_weapon].instance();
-		self.call_deferred("add_child", weapon);
-		weapon.init(1, PlayerStats.damage); 
-		
-		PlayerStats.add_weapon(chosen_weapon);
-		print("shouldve hit here")
-		weapon_list[chosen_weapon] = weapon;
-		
 func update_weapons():
 	var slot_info = PlayerStats.get_display_info()["slots"];
 	
 	var up_slot = slot_info[Constants.UP_ORB];
+
 	
-	var weapon = load("res://Weapons/" + up_slot["type"] + ".tscn").instance();
-	self.call_deferred("add_child", weapon);
-	weapon.init(1, PlayerStats.damage); 
-	
-	
+	for slot in slot_info.values():
+		if (slot["type"] != null):
+			var weapon = load("res://Weapons/" + slot["type"] + ".tscn").instance();
+			self.call_deferred("add_child", weapon);
+			weapon.init(slot["level"], PlayerStats.damage); 
+		
 
 #var slot_info = {
 #"slots" : {
